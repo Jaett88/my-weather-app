fd8c4a605c7016502f37bb014c8890e41b175a0e
Jaett88 <missjaet01@gmail.com>
2024-09-03 10:50:45 +0100

    Updated the icon html, JS and CSS of the weather icon

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 }
 
 function formatDate(date) {
